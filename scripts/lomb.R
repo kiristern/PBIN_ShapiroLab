be8b8ac3804f1002b4e2fea220fa616d8f818aff
kiri <kiri.a.stern@gmail.com>
2025-01-05 09:02:29 -0500

    Update lomb.R
    load from saved .RData

--- scripts/lomb.R
+++ scripts/lomb.R
@@ -1,156 +1,320 @@
-#https://github.com/adriaaulaICM/bbmo_niche_sea/blob/master/src/analysis/seasonality_asvs.R
+# source("lomb_fnc.R")
 
 library(tidyverse)
 library(phyloseq)
 library(lubridate)
 library(lomb)
 
-packageVersion("lomb")
-citation("lomb")
 
-bl.phy <- viral_physeq
+# load from backup
+lomb_env <- new.env() # Create a new environment
+load("arxiv/Large_data/lomb backup.RData", envir = lomb_env) # Load the file into the new environment
+ls(lomb_env)  # List the variables in the new environment
 
-# data_transformation <- function(physeq){
-#   require(tidyverse)
-#   require(phyloseq)
-#   
-#   
-#   
-#   # Relative abundance of OTUs
-#   phy.relab = transform_sample_counts(physeq, function(x) x / sum(x))
-#   
-#   # Log10 abundance of OTUs
-#   phy.log = transform_sample_counts(physeq, function(x) log10(x+1))
-#   
-#   # Rarefied dataset
-#   phy.raref = rarefy_even_depth(physeq,rngseed = 42)
-#   
-#   # Log centered ratio (Aitchison)
-#   geoMean = function(x, na.rm=TRUE){
-#     exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
-#   }
-#   
-#   phy.clr <- transform_sample_counts(physeq, function(x) x+1) %>%
-#     transform_sample_counts(function(x) log(x/geoMean(x)))
-#   
-#   print('if this is not close to zero or zero, something is wrong! Beware')
-#   print(sum(otu_table(phy.clr)[1,]))
-#   
-#   print("Starting a psmelt of a large dataset, this could take a while!")
-#   
-#   return(list( relab = phy.relab, 
-#                log = phy.log,
-#                raref = phy.raref,
-#                clr = phy.clr))
-# }
-# 
-# physeq.list <- data_transformation(bl.phy)
-# 
-# bl.phy.relab <- physeq.list$relab
-# bl.phy.raref <- physeq.list$raref
-# bl.phy.log <- physeq.list$log
-# bl.phy.clr <- physeq.list$clr
-# (virps_helli <- transform(viral_physeq, transform = "hellinger", target = "OTU"))
-# bl.phy.asinh <- transform_sample_counts(bl.phy, function(x) asinh(x))
-# physeq.list <- NULL
+##### Get the data from the new environment
+data_lomb <- lomb_env$lomb.02
+# data_lomb <- lomb_env[["lomb.sea.02"]] # alternative way to get the data
+dplyr::glimpse(data_lomb) # compact view of data
 
-
-psmelt_dplyr = function(physeq) {
-  #Implementation of the psmelt from phyloseq with dplyr
-  # It is indeed faster, without further complications or differences. 
-  # (well, in fact, its way more prone to give errors if the output is not well established, 
-  # it doesn't check anything)
-  
-  sd = data.frame(sample_data(physeq)) %>% 
-    rownames_to_column("Sample")
-  TT = data.frame(as(tax_table(physeq),'matrix')) %>%
-    rownames_to_column("OTU")
-  if(taxa_are_rows(physeq)){
-    otu.table = data.frame(as(otu_table(physeq),"matrix"),
-                           check.names = FALSE) %>%
-      rownames_to_column("OTU")
-  } else {
-    otu.table = data.frame(t(as(otu_table(physeq),"matrix")),
-                           check.names = FALSE) %>%
-      rownames_to_column("OTU")
-  }
-  
-  all <- otu.table %>%
-    left_join(TT, by = "OTU") %>%
-    gather_("Sample", "Abundance", setdiff(colnames(otu.table), "OTU")) %>%
-    left_join(sd, by = "Sample") %>% 
-    select(Sample, Abundance, OTU, everything())
-  
-  return(all)
-}
-
-vs_metadata <- viral_physeq %>% sample_data()
-
-tsdf.0.2 <- bl.phy %>% 
-  psmelt_dplyr() %>% 
-  mutate(decimaldat = decimal_date(as.Date(Date)))
-head(tsdf.0.2)
-tail(tsdf.0.2)
-
-
-#troubleshooting why lomb was not working
-tsdf.0.2$OTU %>% head()
-tsdf.0.2 %>% select(OTU, decimaldat, Abundance) %>%
-  group_by(OTU) %>%
-  summarize( howmany0 = sum(Abundance == 0)) %>%
-  arrange(-howmany0)
-# ~4000+ ASVs that have abundance of 0 in 166 samples
-
-ASVs0 <- tsdf.0.2 %>%
-  select(OTU, decimaldat, Abundance) %>%
-  group_by(OTU) %>%
-  summarize( howmany0 = sum(Abundance == 0)) %>%
-  arrange(-howmany0) %>%
-  filter(howmany0 == 166) %>%
-  pull(OTU)
-
-
-lomb.02 <- tsdf.0.2 %>% 
-  filter(! OTU %in% ASVs0) %>% 
-  split(.$OTU) %>% 
-  map(~randlsp( x =.x$Abundance,
-                times = .x$decimaldat,
-                type = 'period',
-                plot = F))
-
-par(mar=c(1,1,1,1)) #fix error: Error in plot.new() : figure margins too large
-
-lomb.sea.02 <- tibble( asv = names(lomb.02),
-                       pval = map_dbl(lomb.02, ~.x$p.value),
-                       peak = map_dbl(lomb.02, ~.x$peak),
-                       interval  = map(lomb.02, ~.x$peak.at),
+# # select the data (change threshold b/c saved $lomb.sea.02 is empty)
+lomb.sea.02 <- tibble( asv = names(data_lomb),
+                       pval = map_dbl(data_lomb, ~.x$p.value),
+                       peak = map_dbl(data_lomb, ~.x$peak),
+                       interval  = map(data_lomb, ~.x$peak.at),
                        int.min = map_dbl(interval, ~.[[2]]),
                        int.max = map_dbl(interval, ~.[[1]])) %>% 
   mutate( qval = fdrtool::fdrtool(pval, statistic = 'pvalue')$qval) %>% 
-  filter(qval <= 0.01, peak >= 10, int.max <= 2)
-head(lomb.sea.02)
-head(lomb.season <- as.data.frame(lomb.sea.02[,c(1, 2,7)]))
-write.csv(lomb.season, "lomb_seasonality.csv")
+#   filter(qval <= 0.01, peak >= 0.150, int.max <= 2)
+    mutate(seasonality = ifelse(pval < 0.01 & peak >= 0.150 & int.max <= 2,
+                                   'seasonal', 'non_seasonal')) # add column indicating if ASV is seasonal or not
+lomb.sea.02 %>% dplyr::glimpse() # compact view of data
 
-write_rds(lomb.02, 'data/analysis/lomball.rds')
-write_rds(lomb.sea.02, 'data/analysis/lombsea.rds')
+# View data
+lomb.sea.02 %>% 
+  mutate( index = str_replace(asv, 'asv', '') %>% as.integer()) %>% 
+  arrange(index) %>% View()
 
-results.lomb02 <- lomb.02[lomb.sea.02 %>% pull(asv)]
+# filter for seasonal and non-seasonal ASVs
+seasonalASV <- lomb.sea.02 %>% filter(seasonality == 'seasonal') # filter for seasonal ASVs
+nonseasonalASV <- lomb.sea.02 %>% filter(seasonality == 'non_seasonal') # filter for non-seasonal ASVs
 
-# Strip problems
+##### also in saved data (call lomb_env$results.lomb02)
+results.lomb02_seasonal <- data_lomb[seasonalASV %>% pull(asv)]
+dplyr::glimpse(results.lomb02_seasonal) # compact view of data
+
+# plotting
+asv_periods <- map(results.lomb02_seasonal, ~tibble( scanned = .x$scanned,
+                                             power = .x$power)) %>% 
+                                            bind_rows(.id = 'asv') %>% 
+                                            split(.$asv)
+asv_periods %>% head() # check data
+
+# plotting
 lil.strip <- theme(strip.background = element_blank(),
                    strip.text.x =element_text(margin = margin(.05, 0, .1, 0, "cm")))
-
-periodoplots <- map(results.lomb02, ~tibble( scanned = .x$scanned,
-                                             power = .x$power)) %>% 
-  bind_rows(.id = 'asv') %>% 
-  split(.$asv) %>% 
+periodoplots <- asv_periods %>% 
   map(~ggplot(.x, aes(scanned, power)) + 
         geom_line(aes(group = asv)) + 
         facet_wrap(~asv) + 
-        lil.strip)
+        lil.strip
+        )
+periodoplots$ASV_30 # example plot for ASV_30
 
-periodoplots$ASV_302
+
+
+############## 
+# select seasonal ASVs: 10, 103
+# non-seasonal ASVs: 1, 100
+# asv.sel <- str_c('ASV_', c(1,10, 100, 103))
+asv.sel <- str_c('ASV_', c(1:16, 19, 23, 46, 50)) # select from top20 relab plots
+
+# import phyloseq object
+ps <- lomb_env$virps3000filt
+ps <- transform_sample_counts(ps, function(x) x / sum(x))
+
+# check phyloseq object
+tax_table(ps) %>% head() # taxonomy table (ASVs x taxa)
+otu_table(ps) %>% head() # abundance counts (ASVs x samples) -- transformed relab (line 54)
+sample_data(ps) %>% head() # sample metadata (samples x metadata)
+
+# just subset for select ASV
+ps_specific <- psmelt(ps) %>%
+    as_tibble() %>%
+    filter(OTU %in% asv.sel) # filter for specific ASVs
+
+# day of year col
+ps_specific$day_of_year <- as.numeric(format(as.Date(ps_specific$Date), format = "%j"))
+# cumulative day numbers for the start of each month
+num.days.mnt <- c(0,31,28,31,30,31,30,31,31,30,31,30)
+cumnum <- cumsum(num.days.mnt)
+# month order
+month.order <- c('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december')
+
+# https://github.com/adriaaulaICM/bbmo_niche_sea/blob/6cef1b004e75a88a007975f6c5ebc37a40d32b0e/src/figures/sea_explanation.R#L45
+gam.gg <- ggplot(data = ps_specific, aes(day_of_year,Abundance)) + 
+  geom_jitter(aes(color = OTU), alpha = 0.4) + 
+  stat_smooth(aes(x = day_of_year,
+                  group = OTU,
+                  color = OTU),
+              method = "gam",
+              formula = y ~ s(x, k =12, bs = 'cc'),
+              se = F, size = 1,
+              show.legend = F, alpha = 0.7) + 
+  # split into separate facets (plots) for each OTU
+  facet_wrap(~str_c(str_to_upper(OTU), ta5, sep = ', '), scales = 'free_y') + 
+  # display y-axis as percentage
+  scale_y_continuous(labels = scales::percent_format(accuracy = 2L)) +
+  scale_x_continuous(breaks = cumnum,
+                     name = 'Month',
+                     # Display month abbrv first 3 letters
+                     labels = str_to_title(month.order) %>% str_sub(1,3)) +
+  guides(color = "none") + 
+  ylab('Relative abundance') + 
+  lil.strip + 
+  theme(legend.position = 'bottom',)
+gam.gg
+
+periodo.gg <- map(data_lomb, ~tibble( scanned = .x$scanned,
+                                    power = .x$power)) %>%
+  bind_rows(.id = 'asv') %>% 
+  filter(asv %in% asv.sel) %>% 
+  mutate( desc = ifelse(asv == 'ASV_10', 'Strong peak with a periodicity of 1 year',
+                        'No patterns, random walk') %>% as.factor() %>% fct_inorder()) %>% 
+  ggplot(aes(scanned, power)) + 
+  geom_line( aes(group = asv)) + 
+  ylab('Strength recurrence') + 
+  # TODO: have multiple years... is strength reoccurrence computed across all years?
+  xlab('Periods checked (0.083 ~ 1/12 months)') + 
+  facet_wrap(~desc) +
+  lil.strip
+periodo.gg
+# gam.gg + periodo.gg + plot_layout(ncol = 1)
+
+# save plot
+figpath <- 'figs250104' # today's date
+ggsave(filename = str_c(figpath, 'example_seasonality.png'), plot=gam.gg,
+       width = 11, height = 7)
 
 
 
+# https://github.com/adriaaulaICM/bbmo_niche_sea/blob/6cef1b004e75a88a007975f6c5ebc37a40d32b0e/src/figures/seasonal_numbers.Rmd#L388
+ls(lomb_env)
+# seasonality contribution to total relative abundance
+lomb.sea.02 %>% dplyr::glimpse()
+
+# viral ps
+virps <- lomb_env$viral_physeq
+virpsfilt <- lomb_env$virps3000filt
+
+season_relab <- taxa_sums(virps)[lomb.sea.02$asv] %>% sum(na.rm = TRUE)
+total.relab <- taxa_sums(virps) %>% sum()
+total.tested.relab <- taxa_sums(ps) %>% sum()
+
+season.relab * 100 / total.relab
+season.relab * 100 / total.tested.relab
+
+
+sam.sea <- otu_table(bl.phy)[,lombsea$asv] %>% rowSums()
+total.sea <- otu_table(bl.phy) %>% rowSums()
+
+relabs <- sam.sea / total.sea 
+
+mean(relabs)
+sd(relabs)
+
+therel <-  data.frame(this  = relabs, 
+           sample_data(bl.phy))
+
+therel %>% 
+  ggplot( aes(this)) + 
+  geom_histogram()
+
+
+ggplot(therel, 
+       aes(month, this)) + 
+  geom_jitter(alpha = 0.7)
+
+
+
+
+##############
+# check unique taxa
+for (i in colnames(tax_table(ps))) {
+  print(i) # Print the taxonomic rank (column name)
+  # Extract the column and get unique values
+  if (i != "species"){
+    unique_values <- unique(tax_table(ps)[, i])
+    print(unique_values) # Print unique values for the rank
+  }
+}
+# only have Virus (kingdom) & Myoviridae (family) in this dataset
+
+# https://github.com/adriaaulaICM/bbmo_niche_sea/blob/6cef1b004e75a88a007975f6c5ebc37a40d32b0e/src/analysis/seasonality_taxranks.R#L125
+
+# create df with ASV and taxonomic ranks (Kingdom and Family only)
+rawdat <- as(tax_table(ps), "matrix") %>%  
+  as_tibble(rownames = 'asv') %>% 
+  filter(!is.na(ta5)) %>% select(-species) %>%
+#   filter(class %in% c('Alphaproteobacteria',
+#                       'Gammaproteobacteria',
+#                       'Bacteroidia')) %>% 
+  pivot_longer(names_to = 'rank', values_to = 'value', cols = -asv) %>%
+  filter(rank != 'ta2', rank != 'ta3', rank != 'ta4', rank!='ta6') %>%
+  filter(!is.na(value))
+
+# filter for seasonal ASVs
+seasonallevels <- rawdat %>% 
+  filter(asv %in% lomb.sea.02$asv)
+
+# check length
+length(unique(rawdat$asv)) # 585
+length(unique(lomb.sea.02$asv)) # 132
+length(unique(seasonallevels$asv)) # 125
+
+
+# ranks_more10 <-  rawdat  %>%
+#   group_by(rank,value) %>%
+#   filter(n() >= 10) %>%
+#   filter(value %in% seasonallevels$value)  %>% 
+#   distinct(rank,value)
+
+# # note, increasing iteration (5) may be long
+# distributions <- ranks_more10 %>% 
+#   mutate( values = map2(.x = value, .y = rank,
+#                         ~check_at_rank_level(ps, 5, .x, .y)))
+
+# without20max <- ranks_more10 %>% 
+#   mutate( values = map2(.x = value, .y = rank,
+#                         ~check_situation_max(ps, .x, .y)))
+
+# normal <- ranks_more10 %>% 
+#   mutate( values = map2(.x = value, .y = rank,
+#                         ~check_normal(ps, .x, .y)))
+# # look at outputs
+# distributions %>%
+#     unnest(values)
+
+# without20max %>%
+#     unnest(values)
+
+# normal %>%
+#     unnest(values)
+
+
+
+
+
+
+
+# # https://github.com/adriaaulaICM/bbmo_niche_sea/blob/6cef1b004e75a88a007975f6c5ebc37a40d32b0e/src/figures/cohesivity_taxrank.R#L48C1-L56C22
+# dist <- distributions %>% 
+#   ungroup() %>%
+#   unnest(cols = values) #%>% 
+# #     #Some changes in the nomenclature since we have ids repeated 
+# #   mutate( value = case_when(
+# #     rank == 'family' & value == 'D2472' ~ 'D2472_f',
+# #     rank == 'genus' & value == 'D2472' ~ 'D2472_g',
+# #     TRUE ~ value)) %>% 
+# #   valtax_to_italics()
+
+
+
+
+# tax <- as(tax_table(ps), 'matrix') %>% as_tibble(rownames = 'asv')
+
+# corr <- tax %>% 
+#   select(ta1, ta5)  %>% 
+# #   filter(class %in% c('Alphaproteobacteria',
+# #                       'Gammaproteobacteria',
+# #                       'Bacteroidia')) %>% 
+#   pivot_longer(names_to = 'level', values_to = 'value', cols = -ta5) %>% 
+# #   mutate( value = case_when(
+# #     level == 'family' & value == 'D2472' ~ 'D2472_f',
+# #     level == 'genus' & value == 'D2472' ~ 'D2472_g',
+# #     TRUE ~ value)) %>% 
+#   select(-level)
+
+# dist.hist <- distributions %>% 
+#   unnest(values) %>%
+#   mutate(rank = factor(rank, levels = c('ta1', 'ta5')),
+#          significance = ifelse(pval < 0.01 & peak >= 0.154 & int.max <= 2,
+#                                'seasonal', 'non seasonal')) %>% 
+#   left_join(corr, by = 'value') %>% 
+#   mutate(ta5 = ifelse(is.na(ta5), value, ta5))
+
+# global <- dist.hist %>% 
+#   ggplot( aes(peak)) + 
+#   geom_histogram(aes(fill = rank)) +
+#   facet_wrap(~significance, scales = 'free_y', ncol = 1) + 
+#   theme_bw() + 
+# #   lil.strip + 
+# #   scale_fill_ptol() + 
+#   xlab('Peak power of recurrence (strength of signal)') + 
+#   ylab('Count of statistic') #+ 
+# #   leg.bottom  
+# global
+
+
+# seas.label <- data.frame(label = c('', 'seasonal'),
+#                          y = c('ta5', 'ta5'),
+#                          x = c(7.3,16.9))
+  
+# global_res <- ggplot(dist.hist %>%
+#                        mutate(ta5 = factor(ta5,
+#                                              levels = c('k__Virus', 'f__Myoviridae'))),
+#                      aes(x = peak,
+#                          y = rank,
+#                          fill = rank)) + 
+#   geom_vline(xintercept = 10, linetype = 2, color ='tomato3') + 
+# #   geom_density_ridges(show.legend = F) + 
+#   geom_text(data = seas.label,
+#             aes(label = label, x = x, y = y),
+#             inherit.aes = FALSE,
+#             size = 3,
+#             nudge_y = -0.3, color = 'tomato3') + 
+#   facet_wrap(~ta5) + 
+#   scale_y_discrete(label = str_to_title) + 
+# #   scale_fill_ptol() + 
+# #   lil.strip + 
+#   xlab('Peak power of recurrence (strength of signal)') + 
+#   ylab('Rank level') 
